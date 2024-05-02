ce8f655796ec8ddab1d1fc94c17ea5af6c0c11e0
filiprendon <frendont2223@politecnics.barcelona>
2024-05-02 17:52:27 +0200

    .

--- script.js
+++ script.js
@@ -181,71 +181,69 @@ function drop(event) {
 
     let taskElement = document.getElementById(data);
     console.log(taskElement);
-    contextMenuPrint(taskElement);
-    // Crear el SVG para el check verde
-    let checkSVG = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    checkSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    checkSVG.setAttribute("width", "16");
-    checkSVG.setAttribute("height", "16");
-    checkSVG.setAttribute("fill", "green");
-    checkSVG.setAttribute("class", "bi bi-check-square-fill check-icon-svg");
-    checkSVG.setAttribute("viewBox", "0 0 16 16");
+    // contextMenuPrint(taskElement);
 
-    let checkPath = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "path"
-    );
-    checkPath.setAttribute(
-      "d",
-      "M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"
-    );
+    let checkIcon =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-check-square-fill check-icon-svg" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>';
+    let deleteIcon =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg>';
 
-    checkSVG.appendChild(checkPath);
-    taskElement.appendChild(checkSVG);
-  }
-}
-function contextMenuPrint(element) {
-  doneTasks.forEach((element) => {
-    element.addEventListener("contextmenu", function (e) {
-      e.preventDefault();
-      contextMenu.style.display = "block";
-      contextMenu.style.left = e.pageX + "px";
-      contextMenu.style.top = e.pageY + "px";
-      // Guardo el elemento al que se le ha dado click derecho
-      currentElement = this;
-      console.log(currentElement);
+    let iconsContainer = document.createElement("div");
+    iconsContainer.style.display = "flex";
+    iconsContainer.style.justifyContent = "space-between";
+    iconsContainer.innerHTML = checkIcon + deleteIcon;
+
+    taskElement.appendChild(iconsContainer);
+
+    iconsContainer.children[1].addEventListener("click", function () {
+      if (confirm("Are you sure you want to delete this task")) {
+        taskElement.remove();
+      } else {
+        return;
+      }
     });
-  });
+  }
 }
 
-contextMenuPrint();
+// function contextMenuPrint(element) {
+//   doneTasks.forEach((element) => {
+//     element.addEventListener("contextmenu", function (e) {
+//       e.preventDefault();
+//       contextMenu.style.display = "block";
+//       contextMenu.style.left = e.pageX + "px";
+//       contextMenu.style.top = e.pageY + "px";
+//       // Guardo el elemento al que se le ha dado click derecho
+//       currentElement = this;
+//       console.log(currentElement);
+//     });
+//   });
+// }
 
-document.addEventListener("contextmenu", function (e) {
-  e.preventDefault();
-});
+// contextMenuPrint();
 
-document.addEventListener("click", function () {
-  contextMenu.style.display = "none";
-});
+// document.addEventListener("contextmenu", function (e) {
+//   e.preventDefault();
+// });
 
-addDeletePanel.onclick = () => {
-  document.getElementById("delete").appendChild(currentElement);
-  currentElement = null;
-};
+// document.addEventListener("click", function () {
+//   contextMenu.style.display = "none";
+// });
 
-undo.onclick = () => {
-  if (currentElement == null || undoElement == null) {
-    alert("You can´t undo this task");
-  } else {
-    undoElement.appendChild(currentElement);
-  }
+// addDeletePanel.onclick = () => {
+//   document.getElementById("delete").appendChild(currentElement);
+//   currentElement = null;
+// };
 
-  currentElement = null;
-  undoElement = null;
-};
+// undo.onclick = () => {
+//   if (currentElement == null || undoElement == null) {
+//     alert("You can´t undo this task");
+//   } else {
+//     undoElement.appendChild(currentElement);
+//   }
+
+//   currentElement = null;
+//   undoElement = null;
+// };
 
 startBtn.onclick = function () {
   if (!isPaused) {
@@ -267,13 +265,31 @@ addTask.onclick = function () {
     alert("Please add a title and a description");
     return;
   } else {
-    // Añado la clase a la nueva tarea
     let taskId = "task" + idCounter;
     idCounter++;
-    let newTask = `<li class="task new-task" id=${taskId} draggable="true" draggable="true" ondragstart="drag(event)">
-        <h5><b>${title}</b></h5><br><p>${description}</p><p>${currentDate()}</p></li>`;
-    document.getElementById("to-do").innerHTML += newTask;
-    // Borrar el contenido de la tarea que acabamos de añadir
+
+    let newTask = document.createElement("li");
+    newTask.className = "task new-task";
+    newTask.id = taskId;
+    newTask.draggable = true;
+    newTask.addEventListener("dragstart", drag);
+
+    let titleElement = document.createElement("h5");
+    titleElement.innerHTML = "<b>" + title + "</b>";
+
+    let descriptionElement = document.createElement("p");
+    descriptionElement.textContent = description;
+
+    let dateElement = document.createElement("p");
+    dateElement.textContent = currentDate();
+
+    newTask.appendChild(titleElement);
+    newTask.appendChild(document.createElement("br"));
+    newTask.appendChild(descriptionElement);
+    newTask.appendChild(dateElement);
+
+    document.getElementById("to-do").appendChild(newTask);
+
     document.getElementById("taskText").value = "";
     document.getElementById("description").value = "";
     document.getElementById("category").value = "0";
